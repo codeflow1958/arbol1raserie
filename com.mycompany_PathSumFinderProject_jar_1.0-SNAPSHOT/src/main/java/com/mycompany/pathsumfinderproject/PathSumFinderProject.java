fd7f77c2d74550358298f97d96b7e91725d47a78
codeflow1958 <nickylds1958@gmail.com>
2025-04-29 22:49:12 -0600

    cambios del paquete

---
+++ com.mycompany_PathSumFinderProject_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/pathsumfinderproject/PathSumFinderProject.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pathsumfinderproject;
+
+import java.util.List;
+import parcial.sum.PathSumFinder;
+import parcial.sum.TreeNode;
+
+/**
+ *
+ * @author 50241
+ */
+public class PathSumFinderProject {
+
+    public static void main(String[] args) {
+        
+        PathSumFinder finder = new PathSumFinder();
+    Integer[] values = {5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1};
+    int targetSum = 22;
+
+    TreeNode root = finder.buildTree(values);
+    List<List<Integer>> paths = finder.pathSum(root, targetSum);
+
+    System.out.println("Paths where sum equals target:");
+    for (List<Integer> path : paths) {
+        System.out.println(path);
+    }
+        
+    }
+}
---
+++ com.mycompany_PathSumFinderProject_jar_1.0-SNAPSHOT/src/main/java/parcial/sum/TreeNode.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parcial.sum;
+
+/**
+ *
+ * @author 50241
+ */
+public class TreeNode {
+    
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+    
+}
